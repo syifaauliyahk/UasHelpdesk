54556817650ad8583dac20d49f9e7f6417c68639
syifaauliyahk <157342891+syifaauliyahk@users.noreply.github.com>
2025-12-03 18:36:13 +0700

    Update Hibernate Ticket.java

--- src/main/java/com/example/demo/model/Ticket.java
+++ src/main/java/com/example/demo/model/Ticket.java
@@ -3,27 +3,27 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
+@Entity //Hibernate (memetakan class java ke tabel database)
 public class Ticket {
 
-    @Id
+    @Id //Hibernate (memetakan class java ke tabel database)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String pelapor;       // Nama yang lapor
-    private String deskripsi;     // Masalahnya apa (misal: Wifi Mati)
+    private String pelapor;       
+    private String deskripsi;     
     private String prioritas;     // HIGH / MEDIUM / LOW
     
     // BPM State: OPEN -> IN_PROGRESS -> CLOSED
     private String status;
     
-    private String teknisi;       // Siapa yang ngerjain
+    private String teknisi;       
     private LocalDateTime waktuLapor;
 
     // Constructor kosong
     public Ticket() {}
 
-    // Getters & Setters Manual (Biar aman kalau ga pake Lombok)
+    // Getters & Setters Manual 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getPelapor() { return pelapor; }
@@ -39,3 +39,4 @@ public class Ticket {
     public LocalDateTime getWaktuLapor() { return waktuLapor; }
     public void setWaktuLapor(LocalDateTime waktuLapor) { this.waktuLapor = waktuLapor; }
 }
+
